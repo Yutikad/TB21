0a8489672a03b0da150041b898bc0252a4d61f1a
Yutikad <142665985+Yutikad@users.noreply.github.com>
2023-08-22 14:52:07 +0530

    Update A.java

--- GitHub/src/A.java
+++ GitHub/src/A.java
@@ -1,14 +1,16 @@
 
 public class A {
+
+	public void m1()
+			{
+			System.out.println("m1");
+			}
 	
 	public static void main(String[] args) {
 		
 		System.out.println("GitHub");
 
-		public void m1()
-			{
-			System.out.println("m1");
-			}
+		
 	}
 
 }
